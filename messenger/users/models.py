0c56aa88155ce5ecb6a6ef1bf6f8e6e0057160a8
Nikita Tarasov <nikitatarasov987@gmail.com>
2019-11-05 13:48:40 +0300

    Модель пользователя теперь наследуется от AbstractUser

--- messenger/users/models.py
+++ messenger/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
-class User (AbstractBaseUser):
+class User (AbstractUser):
     chat = models.ManyToManyField('chats.Chat')
     name = models.CharField(max_length=64)
     nick = models.CharField(max_length=64)
