f556d8c93f40ca09b425f02e6f784cddf8c7f87c
Nikita Tarasov <nikitatarasov987@gmail.com>
2019-11-30 20:08:46 +0300

    small fixes in download_file meth

--- messenger/chats/views.py
+++ messenger/chats/views.py
@@ -170,13 +170,12 @@ def download_file(request, chat_id=None):
         return JsonResponse({'Error': 'ID is None'}, status=400)
     image_name = request.GET.get('image_name')
     if image_name is None:
-        return JsonResponse({'Error': 'invalid image key {}'.format(image_key)}, status=400)
-    image_path ='home/nikita/BackendMail/messenger/chats/images/{}'.format(image_name)
+        return JsonResponse({'Error': 'invalid image name {}'.format(image_name)}, status=400)
     chat = Chat.objects.filter(id=chat_id).first()
     if chat is None:
         return JsonResponse({'Error': 'No such Chat {}'.format(chat_id)}, status=404)
 
-    image = Message.objects.filter(chat_id=chat_id).filter(image__contains=image_path).first()
+    image = Message.objects.filter(chat_id=chat_id).filter(image__contains=image_name).first()
     if image is None:
          return JsonResponse({'Error': 'Image with name {} have not been found'.format(image_name)}, status=404)
 
